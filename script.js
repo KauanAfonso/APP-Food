79c54f7298a31e7e87d6bc02c8a619d0efc69f51
KauanAfonso <kauanafon3@gmail.com>
2024-01-24 20:15:13 -0300

    Melhorando o menu

--- script.js
+++ script.js
@@ -7,3 +7,66 @@ function toggleMenu() {
     }
   }
   
+
+  const imagem1 = document.getElementById('img1')
+  const imagem2 = document.getElementById('img2')
+  const imagem3 = document.getElementById('img3')
+  const imagem4 = document.getElementById('img4')
+  const imagem5 = document.getElementById('img5')
+
+
+  imagem1.addEventListener('click' , function(){
+ 
+    imagem1.style.opacity = '0.3'
+    imagem2.style.opacity = '1'
+    imagem3.style.opacity = '1'
+    imagem4.style.opacity = '1'
+    imagem5.style.opacity = '1'
+
+  })
+
+
+  imagem2.addEventListener('click' , function(){
+ 
+    imagem1.style.opacity = '1'
+    imagem2.style.opacity = '0.3'
+    imagem3.style.opacity = '1'
+    imagem4.style.opacity = '1'
+    imagem5.style.opacity = '1'
+
+  })
+
+
+  
+  imagem3.addEventListener('click' , function(){
+ 
+    imagem1.style.opacity = '1'
+    imagem2.style.opacity = '1'
+    imagem3.style.opacity = '0.3'
+    imagem4.style.opacity = '1'
+    imagem5.style.opacity = '1'
+
+  })
+
+
+  
+  imagem4.addEventListener('click' , function(){
+ 
+    imagem1.style.opacity = '1'
+    imagem2.style.opacity = '1'
+    imagem3.style.opacity = '1'
+    imagem4.style.opacity = '0.3'
+    imagem5.style.opacity = '1'
+
+  })
+
+ 
+  imagem5.addEventListener('click' , function(){
+ 
+    imagem1.style.opacity = '1'
+    imagem2.style.opacity = '1'
+    imagem3.style.opacity = '1'
+    imagem4.style.opacity = '1'
+    imagem5.style.opacity = '0.3'
+
+  })
