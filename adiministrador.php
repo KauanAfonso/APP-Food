0b796fc514ef5b1fcf627975f75d88862b0b1c6c
KauanAfonso <kauanafon3@gmail.com>
2024-02-17 16:36:18 -0300

    Criando um crud

--- adiministrador.php
+++ adiministrador.php
@@ -1,1 +1,30 @@
-<h1>Administrador</h1>
+<?php 
+
+
+
+session_start();
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<h1>Administrador</h1> 
+
+<a href="editarProdutos.php">Editar produtos</a>
+<a href="">Conferir pedidos</a>
+<a href="">Mensagens</a>
+
+
+</body>
+</html>
--- tela.inicial.php
+++ tela.inicial.php
@@ -59,11 +59,12 @@ $queryProdutosDocesFinais = $conn->query($queryProdutosDoces);
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700&family=Rubik+Moonrocks&display=swap" rel="stylesheet">
-<script src="script.js" defer></script>
-<script src='carrinho.js'></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700&family=Rubik+Moonrocks&display=swap"
+    rel="stylesheet">
+  <script src="script.js" defer></script>
+  <script src='carrinho.js'></script>
 </head>
 
 
@@ -72,7 +73,9 @@ $queryProdutosDocesFinais = $conn->query($queryProdutosDoces);
   <div id="conteudoMenuMobile">
 
     <nav>
-      <h5>SEJA BEM-VINDO <?php echo " " .  $nome . "!" ?>;</h5>
+      <h5>SEJA BEM-VINDO
+        <?php echo " " .  $nome . "!" ?>;
+      </h5>
 
       <p id="fecharMenu" style="font-size: 1rem; cursor: pointer;" onclick="toggleMenu()">X</p>
     </nav>
@@ -91,7 +94,9 @@ $queryProdutosDocesFinais = $conn->query($queryProdutosDoces);
 
       <nav>
         <div class="nome">
-          <h1>SEJA BEM-VINDO<?php echo ": " . " <h1>$nomeMaiusculo</h1>"  ?></h1>
+          <h1>SEJA BEM-VINDO
+            <?php echo ": " . " <h1>$nomeMaiusculo</h1>"  ?>
+          </h1>
         </div>
 
         <button id="mn-hamburguer" onclick="toggleMenu()">≣</button>
@@ -115,11 +120,11 @@ $queryProdutosDocesFinais = $conn->query($queryProdutosDoces);
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div class="offcanvas-body">
-          <ul id="produtosNoCarrinho"></ul>
+            <ul id="produtosNoCarrinho"></ul>
 
-          <textarea id="mensagem" rows="5" cols="28" placeholder='Tirar cebola...'></textarea>
-          <h5 id='totalCompra'>Total: </h5>
-          <button type="button" class="btn btn-success">Finalizar Compra</button>
+            <textarea id="mensagem" rows="5" cols="28" placeholder='Tirar cebola...'></textarea>
+            <h5 id='totalCompra'>Total: </h5>
+            <button type="button" class="btn btn-success">Finalizar Compra</button>
           </div>
         </div>
 
@@ -149,7 +154,7 @@ $queryProdutosDocesFinais = $conn->query($queryProdutosDoces);
 
     <h1 style="position: absolute; top: 420px; left: calc(50% - 81.75px); ">Cardapio</h1>
 
-    <div class="position-div" >
+    <div class="position-div">
       <div>
         <img class="img1" id="img1" src="imagens/carrossel/Group 13.png" alt="">
         <img class="img1" id="img2" style="margin-left: 200px;" src="imagens/carrossel/Group 14.png" alt="">
@@ -160,10 +165,10 @@ $queryProdutosDocesFinais = $conn->query($queryProdutosDoces);
         <img class="img3" id="img5" src="imagens/carrossel/Group 17.png" alt="">
       </div>
     </div>
-<p class='todosProdutos' style="margin-top: 580px;display:;">
+    <p class='todosProdutos' style="margin-top: 580px;display:;">
 
 
-<?php
+      <?php
 
 // Se a consulta retornar mais de uma linha
 if ($produtosFinais->num_rows > 0) {
@@ -199,11 +204,11 @@ if ($produtosFinais->num_rows > 0) {
 ?>
 
 
-  
-</p>
+
+    </p>
 
 
-<?php 
+    <?php 
 
 if($queryProdutosLanchesFinais->num_rows > 0){
 
@@ -241,7 +246,7 @@ while($row = $queryProdutosLanchesFinais->fetch_assoc()){
 ?>
 
 
-<?php 
+    <?php 
 
 if($queryProdutosPasteisFinais->num_rows > 0){
 
@@ -279,7 +284,7 @@ while($row = $queryProdutosPasteisFinais->fetch_assoc()){
 ?>
 
 
-<?php 
+    <?php 
 
 if($queryProdutosBatatasFinais->num_rows > 0){
 
@@ -317,7 +322,7 @@ while($row = $queryProdutosBatatasFinais->fetch_assoc()){
 ?>
 
 
-<?php
+    <?php
 
 
 if($queryProdutosDocesFinais->num_rows >0){
@@ -377,122 +382,122 @@ if($queryProdutosDocesFinais->num_rows >0){
 <!----------------codigo em javascript para manipular o dom:--------------------->
 
 <script>
- 
-var produtos = document.querySelectorAll('.container');
-var Lanches = document.querySelectorAll('.container2');
-var Pasteis = document.querySelectorAll('.container3');
-var Batatas = document.querySelectorAll('.container4');
-var Doces = document.querySelectorAll('.container5');
 
-function esconder(elemento){
-  elemento.forEach(element => {
+  var produtos = document.querySelectorAll('.container');
+  var Lanches = document.querySelectorAll('.container2');
+  var Pasteis = document.querySelectorAll('.container3');
+  var Batatas = document.querySelectorAll('.container4');
+  var Doces = document.querySelectorAll('.container5');
 
-    element.style.display = 'none'
-      
-  });
-}
+  function esconder(elemento) {
+    elemento.forEach(element => {
+
+      element.style.display = 'none'
+
+    });
+  }
 
 
-document.getElementById('img5').addEventListener('click' , function(){
-    
-  
-    produtos.forEach(function(element) {
-    if(element.style.display == 'none'){
-      element.style.display = 'inline-block'; 
-    }else{
-      element.style.display = 'none'; 
-    }
-   
+  document.getElementById('img5').addEventListener('click', function () {
+
+
+    produtos.forEach(function (element) {
+      if (element.style.display == 'none') {
+        element.style.display = 'inline-block';
+      } else {
+        element.style.display = 'none';
+      }
+
     });
 
-  esconder(Doces)
-  esconder(Lanches)
-  esconder(Pasteis)
-  esconder(Batatas)
-})
+    esconder(Doces)
+    esconder(Lanches)
+    esconder(Pasteis)
+    esconder(Batatas)
+  })
 
 
 
 
-document.getElementById('img2').addEventListener('click' , function(){
+  document.getElementById('img2').addEventListener('click', function () {
 
-    Lanches.forEach(function(element) {
-    if(element.style.display == 'none'){
-      element.style.display = 'inline-block'; 
-    }else{
-      element.style.display = 'none'; 
-    }
-   
+    Lanches.forEach(function (element) {
+      if (element.style.display == 'none') {
+        element.style.display = 'inline-block';
+      } else {
+        element.style.display = 'none';
+      }
+
     });
 
-  esconder(produtos)
-  esconder(Doces)
-  esconder(Pasteis)
-  esconder(Batatas)
-})
+    esconder(produtos)
+    esconder(Doces)
+    esconder(Pasteis)
+    esconder(Batatas)
+  })
 
 
-document.getElementById('img3').addEventListener('click' , function(){
+  document.getElementById('img3').addEventListener('click', function () {
 
-Pasteis.forEach(function(element) {
-if(element.style.display == 'none'){
-  element.style.display = 'inline-block'; 
-}else{
-  element.style.display = 'none'; 
-}
+    Pasteis.forEach(function (element) {
+      if (element.style.display == 'none') {
+        element.style.display = 'inline-block';
+      } else {
+        element.style.display = 'none';
+      }
 
-});
+    });
 
-  esconder(produtos)
-  esconder(Lanches)
-  esconder(Doces)
-  esconder(Batatas)
-
-})
-
-
-document.getElementById('img1').addEventListener('click' , function(){
-
-Batatas.forEach(function(element) {
-if(element.style.display == 'none'){
-  element.style.display = 'inline-block'; 
-}else{
-  element.style.display = 'none'; 
-}
-
-});
-
-  esconder(produtos)
-  esconder(Lanches)
-  esconder(Pasteis)
-  esconder(Doces)
-})
-
-
-document.getElementById('img4').addEventListener('click' , function(){
-
-  Doces.forEach(function(element){
-  if(element.style.display == 'none'){
-    element.style.display = 'inline-block'; 
-}else{
-  element.style.display = 'none'; 
-  
-}
-
+    esconder(produtos)
+    esconder(Lanches)
+    esconder(Doces)
+    esconder(Batatas)
 
   })
 
-  esconder(produtos)
-  esconder(Lanches)
-  esconder(Pasteis)
-  esconder(Batatas)
 
- 
-}
+  document.getElementById('img1').addEventListener('click', function () {
+
+    Batatas.forEach(function (element) {
+      if (element.style.display == 'none') {
+        element.style.display = 'inline-block';
+      } else {
+        element.style.display = 'none';
+      }
+
+    });
+
+    esconder(produtos)
+    esconder(Lanches)
+    esconder(Pasteis)
+    esconder(Doces)
+  })
+
+
+  document.getElementById('img4').addEventListener('click', function () {
+
+    Doces.forEach(function (element) {
+      if (element.style.display == 'none') {
+        element.style.display = 'inline-block';
+      } else {
+        element.style.display = 'none';
+
+      }
+
+
+    })
+
+    esconder(produtos)
+    esconder(Lanches)
+    esconder(Pasteis)
+    esconder(Batatas)
+
+
+  }
 
 
 
-)
+  )
 
 
 </script>
@@ -503,22 +508,22 @@ document.getElementById('img4').addEventListener('click' , function(){
   var totalDoCarrinho = document.getElementById("totalCompra");
   var contador = 0;
 
- 
+
 
   btnCarrinho.forEach(element => {
-      element.addEventListener('click', function(){
-        // Obtenha o ID do produto a partir do botão clicado
-        var idDoProduto = element.getAttribute('data-id-produto');
-        // Use o ID do produto para acessar o elemento correspondente
-        var produtosDaLoja = document.getElementById("produto" + idDoProduto);
-        
-        var nomeDoProduto = produtosDaLoja.querySelector('.card-title').textContent;
-        var imgDoProduto = produtosDaLoja.querySelector('.col-md-4 img').getAttribute('src');
-        var precoDoProduto = parseFloat(produtosDaLoja.querySelector('.card-subtitle').textContent);
-        var descricao = produtosDaLoja.querySelector('.card-text').textContent;
+    element.addEventListener('click', function () {
+      // Obtenha o ID do produto a partir do botão clicado
+      var idDoProduto = element.getAttribute('data-id-produto');
+      // Use o ID do produto para acessar o elemento correspondente
+      var produtosDaLoja = document.getElementById("produto" + idDoProduto);
+
+      var nomeDoProduto = produtosDaLoja.querySelector('.card-title').textContent;
+      var imgDoProduto = produtosDaLoja.querySelector('.col-md-4 img').getAttribute('src');
+      var precoDoProduto = parseFloat(produtosDaLoja.querySelector('.card-subtitle').textContent);
+      var descricao = produtosDaLoja.querySelector('.card-text').textContent;
 
 
-carrinho.innerHTML += `
+      carrinho.innerHTML += `
         <li>
           <div class="card mb-3" style="max-width: 400px;" >
             <div class="row g-0">
@@ -538,31 +543,31 @@ carrinho.innerHTML += `
         </li>`;
 
 
-alert("Produto adicionado no carrinho: " + nomeDoProduto);
+      alert("Produto adicionado no carrinho: " + nomeDoProduto);
 
 
-contador+= precoDoProduto
-console.log(contador)
+      contador += precoDoProduto
+      console.log(contador)
 
 
-totalDoCarrinho.textContent = "R$" + contador.toLocaleString('pt-BR', { minimumFractionDigits: 2 });
-  
+      totalDoCarrinho.textContent = "R$" + contador.toLocaleString('pt-BR', { minimumFractionDigits: 2 });
 
 
 
- 
-      });
 
-      
+
+    });
+
+
   });
 
-  function removerProduto(){
+  function removerProduto() {
     var produtosNoCarrinho = document.querySelectorAll('#produtosNoCarrinho li');
 
     // Verifique se há itens no carrinho antes de prosseguir
     if (produtosNoCarrinho.length === 0) {
-        alert('Carrinho vazio!');
-        return;
+      alert('Carrinho vazio!');
+      return;
     }
 
     // Encontre o último item no carrinho e remova-o
@@ -576,7 +581,7 @@ totalDoCarrinho.textContent = "R$" + contador.toLocaleString('pt-BR', { minimumF
     // Atualize o total do carrinho exibindo-o novamente na página
     totalDoCarrinho.textContent = "R$" + contador.toLocaleString('pt-BR', { minimumFractionDigits: 2 });
 
-}
+  }
 
 
   btnComprar = document.querySelectorAll('li .btn.btn-danger').forEach(removerProduto)
@@ -584,12 +589,10 @@ totalDoCarrinho.textContent = "R$" + contador.toLocaleString('pt-BR', { minimumF
 
   console.log(produtosNoCarrinho)
 
- 
+
 
 </script>
 
 
 
 </html>
-
-
